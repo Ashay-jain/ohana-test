29fe8524a45c17b69aabc076061002b10a93003a
anupnandeda <87858861+anupnandeda@users.noreply.github.com>
2021-07-23 17:29:39 +0530

    Delete scripts directory

--- scripts/install_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# yum update -y
-# yum groupinstall -y “PHP Support”   
-# yum install -y php-mysql  
-# yum install -y httpd
-# yum install -y php-fpm  
-# cd /var/www/html/ohana
-# composer install
