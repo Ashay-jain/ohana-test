c450fbcf7292e28140c4957370b354cf0d2a5689
anupnandeda <87858861+anupnandeda@users.noreply.github.com>
2021-07-23 17:01:51 +0530

    Create install_dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# yum update -y
+# yum groupinstall -y “PHP Support”   
+# yum install -y php-mysql  
+# yum install -y httpd
+# yum install -y php-fpm  
+# cd /var/www/html/ohana
+# composer install
